caf98cfa573df44ce942bf8439affcd76388ae74
Iryna Jurkiv <iryna.jurkiv@gmail.com>
2020-01-24 10:29:56 +0000

    Solved errors

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -5,12 +5,16 @@ describe Booking do
 
   describe '.create' do
     it "creates a new booking request" do
-      booking_request = Booking.create('2020-03-01', '2020-03-03', 2)
+      owner = User.create(name: 'Owner', email: 'test@example.com', password: 'password123')
+      renter = User.create(name: 'Renter', email: 'tester@example.com', password: 'password123')
+      space = Space.create(name: 'Test Name', description: 'Test Description',
+      price: 50,available_from: '2020-01-01', available_to: '2020-01-30', user_id: owner.id)
+      booking_request = Booking.create(start_date: '2020-03-01', end_date: '2020-03-03', renter: renter.id, space_id: space.id)
 
       expect(booking_request).to be_a Booking
-      expect(booking_request.owner).to eq 1
-      expect(booking_request.space_id).to eq 2
-      expect(booking_request.renter).to eq 6
+      expect(booking_request.owner).to eq owner.id
+      expect(booking_request.space_id).to eq space.id
+      expect(booking_request.renter).to eq renter.id
       expect(booking_request.start_date).to eq '2020-03-01'
       expect(booking_request.end_date).to eq '2020-03-03'
       expect(booking_request.booking_status).to eq "reserved"
--- spec/features/booking_request_spec.rb
+++ spec/features/booking_request_spec.rb
@@ -1,8 +1,9 @@
 feature 'request booking' do
   scenario 'user can request to book a space' do
-    visit('/space/id')
-    fill_in :start_date, with: "2020-02-01"
-    fill_in :end_date, with: "2020-02-04"
+    
+    visit('/space/:id')
+    fill_in(:start_date, with: "2020-02-01")
+    fill_in(:end_date, with: "2020-02-04")
     click_button("Request")
 
     expect(page.status_code).to eq 200
