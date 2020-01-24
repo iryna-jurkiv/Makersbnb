5cf11ca723ceaba686b1c45ffbf279cc6f7d307b
Carmen-Alina <carmen.panu@gmail.com>
2020-01-24 09:24:48 +0000

    Sign in and Sign out completed

--- lib/users.rb
+++ lib/users.rb
@@ -17,6 +17,9 @@ class User
 
   def self.authenticate(email:, password:)
     result =DatabaseConnection.query("SELECT * FROM users WHERE email = '#{email}'")
+    return unless result.any?
+    return unless BCrypt::Password.new(result[0]['password']) == password
+
     user = User.new(id: result[0]['id'], name: result[0]['name'], email: result[0]['email'], password: result[0]['password'])
   end
 
